7239a8633813bdc862be0d2875a8ed766fd24ccc
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2021-10-29 22:02:05 +0530

    Some security changes....

--- routes/auth.js
+++ routes/auth.js
@@ -8,14 +8,14 @@ const {signUp, login, getUser, fetchUser, updateDiaryName} = require('../control
 //POST: Route for signing up a new User. For performing this you need not to be signed in or validated user.
 router.post('/user/signup',[
     body('firstname').isLength({min: 2}).withMessage('Your first name should be at least 2 characters long'),
-    body('username').isLength({min:4}).withMessage('The username must be at least 4 characters long'),
+    body('username').isLength({min:4, max: 30}).withMessage('The username must be at least 4 characters long'),
     body('password').isLength({min: 8, max: 20}).withMessage('The password must be at least 8 characters long and at max it can be 20 characters long')
 ], signUp)
 
 
 //POST: Route for logging in an user. For performing this you need not to be signed in.
 router.post('/user/login',[
-    body('username').isLength({min:4}).withMessage('The username must be at least 4 characters long'),
+    body('username').isLength({min:4, max: 30}).withMessage('The username must be at least 4 characters long'),
     body('password').isLength({min: 8, max: 20}).withMessage('The password must be at least 8 characters long and at max it can be 20 characters long')
 ], login)
 
